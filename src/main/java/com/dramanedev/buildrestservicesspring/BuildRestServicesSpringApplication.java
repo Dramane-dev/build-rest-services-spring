b6599fdf02c262b968d844b5936c1ccfca140817
Dramane <Dramane.dev@gmail.com>
2022-12-15 13:40:30 +0100

    initial commit

---
+++ src/main/java/com/dramanedev/buildrestservicesspring/BuildRestServicesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dramanedev.buildrestservicesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildRestServicesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildRestServicesSpringApplication.class, args);
+    }
+
+}
