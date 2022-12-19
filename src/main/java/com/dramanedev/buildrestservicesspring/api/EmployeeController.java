a5e44166721456952e60df757039e088cc40fd15
Dramane <Dramane.dev@gmail.com>
2022-12-19 13:18:44 +0100

    add employee controller

---
+++ src/main/java/com/dramanedev/buildrestservicesspring/api/EmployeeController.java
@@ -0,0 +1,51 @@
+package com.dramanedev.buildrestservicesspring.api;
+
+import com.dramanedev.buildrestservicesspring.model.EmployeeEntity;
+import com.dramanedev.buildrestservicesspring.service.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/employees")
+    List<EmployeeEntity> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    EmployeeEntity addEmployee(@RequestBody EmployeeEntity employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    EmployeeEntity getById(@PathVariable Long employeeId) {
+        return employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new EmployeeNotFoundException(employeeId));
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    EmployeeEntity update(@RequestBody EmployeeEntity employeeUpdateInfos, @PathVariable Long employeeId) {
+        return employeeRepository.findById(employeeId)
+                .map(employee -> {
+                    employee.setName(employeeUpdateInfos.getName());
+                    employee.setRole(employeeUpdateInfos.getRole());
+                    return employeeRepository.save(employee);
+                })
+                .orElseGet(() -> {
+                    employeeUpdateInfos.setId(employeeId);
+                    return employeeRepository.save(employeeUpdateInfos);
+                });
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    void delete(@PathVariable Long employeeId) {
+        employeeRepository.deleteById(employeeId);
+    }
+}
